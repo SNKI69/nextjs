140ea692bf662b8da78026841b5adbed35f1a8a1
samrat-gh <samratghimire32@gmail.com>
2022-12-27 08:44:56 +0545

    Simple Blog Site

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+import Date from '../components/date'
 
 import { getSortedPostsData } from '../lib/posts';
 
@@ -32,11 +34,13 @@ export default function Home({allPostsData}) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
+
+              <Link href={`/posts/${id}`}> {title} </Link>
+               <br />
+              <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+              </small>
+
             </li>
           ))}
         </ul>
@@ -45,29 +49,3 @@ export default function Home({allPostsData}) {
   );
 }
 
-
-// ================================================
-
-// export default function Home({ allPostsData }) {
-//   return (
-//     <Layout home>
-//       {/* Keep the existing code here */}
-
-//       {/* Add this <section> tag below the existing <section> tag */}
-//       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-//         <h2 className={utilStyles.headingLg}>Blog</h2>
-//         <ul className={utilStyles.list}>
-//           {allPostsData.map(({ id, date, title }) => (
-//             <li className={utilStyles.listItem} key={id}>
-//               {title}
-//               <br />
-//               {id}
-//               <br />
-//               {date}
-//             </li>
-//           ))}
-//         </ul>
-//       </section>
-//     </Layout>
-//   );
-// }
